da2266b60cbee5c77e024795b15b91dc9716f6f5
gmunhoz0810 <gabrielmunhoz12345@gmail.com>
2024-09-09 05:32:33 -0300

    add deployment on render

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://gmunhoz0810.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
